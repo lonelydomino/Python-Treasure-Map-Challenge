90dccd070094f937d96d483760a6fbda4189f5da
Michael Martinez <66656582+lonelydomino@users.noreply.github.com>
2022-06-17 21:04:14 +0000

    Finishing

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ position = input("Where do you want to put the treasure? ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
-
-
+map[int(position[1]) - 1][int(position[0])- 1] = 'X'
 
 
 
